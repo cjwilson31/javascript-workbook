97a826c6fd5c3d838ef704db41f70b99b9b2bcc3
Carla Wilson <cjwilson31@gmail.com>
2018-10-25 19:03:09 -0500

    one

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,11 @@
+"use strict"
+const theList = ['jackie','marion','abbey','john','jim']
+
+const printListOfOlympians = ()=> {
+    theList.forEach(function(item, index, array) {
+        
+
+}}
+
+
+console.log(printListOfOlympians);
