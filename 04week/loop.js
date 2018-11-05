3248f507ef0d297f76f98a9dd97d35c4017a35e5
Carla Wilson <cjwilson31@gmail.com>
2018-11-05 10:42:59 -0600

    started

---
+++ 04week/loop.js
@@ -0,0 +1,27 @@
+// Complete each of the following exercises.
+// for loop
+// Use a for loop to console.log each item in the array carsInReverse.
+const carsInReverse = ['acura','jeep','mercedes','dodge'];
+
+for (var i = 0; i < carsInReverse.length; i ++){
+  console.log(i);
+}
+
+// for...in loop
+// Create an object (an array with keys and values) called persons with the following data:
+// firstName: "Jane"
+// lastName: "Doe"
+// birthDate: "Jan 5, 1925"
+// gender: "female"
+// Use a for...in loop to console.log each key.
+// Then use a for...in loop and if state to console.log the value associated with the key birthDate.
+// while loop
+// Use a for loop to console.log the numbers 1 to 1000.
+// do...while loop
+// Use a do...while loop to console.log the numbers from 1 to 1000.
+// When is a for loop better than a while loop?
+// /in general a while loop is used if you want an action to repeat itself until a certain condition is met i.e. if statement. 
+// /An for loop is used when you want to iterate through an object. i.e. iterate through an array.
+// // How is the readability of the code affected?
+// What is the difference between a for loop and a for...in loop?
+// What is the difference between a while loop and a do...while loop?
